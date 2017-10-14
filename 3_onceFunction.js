88af3788bf46a18c24c1224574eeb858400be5f7
AGCB <oooalexoooo@gmail.com>
2017-10-14 11:08:26 -0700

    Update 3_onceFunction.js

--- 3_onceFunction.js
+++ 3_onceFunction.js
@@ -2,7 +2,7 @@
    WRITE A FUNCTION THAT CAN ONLY RUN ONCE
 */
 let once = () => {
-//even though we are redefining the once function on line 8, it wont affect 
+//even though we are redefining the once function on line 7, it wont affect 
 //our first run. 
 once = () => {return 'sorry'}
 return 42;
@@ -13,3 +13,6 @@ return 42;
 console.log(once()) //returns 42
 console.log(once()) //returns 'sorry'
 console.log(once()) //returns 'sorry'
+
+/* a different idea for line 7 is to just set once to undefined. 
+   */
